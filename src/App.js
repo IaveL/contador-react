ecdf5ac98ff94d5a3f5b564c321eff696a0cb623
Igor Avelino <igoravel21@gmail.com>
2022-02-16 14:47:44 -0300

    uploading project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    count: 0
+  };
+  // cria uma arrow function para ADICIONAR um número. SE(?) o número dentro do state for MENOR ou IGUAL a 9 -> somar 1. ELSE(:) mantém o número atual
+
+  add = () => {
+    this.setState({
+      count: this.state.count <= 9 ? this.state.count + 1 : this.state.count
+    });
+  };
+
+  //função para remover. se o número for MAIOR que zero (para não acabar com n negativo) diminui um.
+
+  remove = () => {
+    this.setState({
+      count: this.state.count > 0 ? this.state.count - 1 : this.state.count
+    })};
+
+  clear = () => {
+    this.setState({
+      count: 0
+    })
+  };
+
+render(){
+  return(
+    <div className="MainBox">
+        <h1>Contador!</h1>
+        <div className="Counter">
+          <button onClick={this.add}>+</button>
+          <h1>{this.state.count}</h1>
+          <button onClick={this.remove}>-</button>
+          <button onClick={this.clear}>Clear</button>
+        </div>
+      </div>
+  )
 }
 
+  
+}
 export default App;
